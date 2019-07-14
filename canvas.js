5ba17fc4a6508c496475a9f15d995821242dc431
DevAccNiclas <nxclxs.rist@gmail.com>
2019-07-15 01:20:08 +0200

    first fourier series

--- canvas.js
+++ canvas.js
@@ -6,10 +6,7 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 // Variables
-const mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
-}
+const mouse = { x: innerWidth / 2, y: innerHeight / 2 }
 
 const colors = [
   '#2185C5',
@@ -22,33 +19,37 @@ const colors = [
 addEventListener('mousemove', event => {
   mouse.x = event.clientX;
   mouse.y = event.clientY;
-})
+});
 
 addEventListener('resize', () => {
   canvas.width = innerWidth;
   canvas.height = innerHeight;
 
   init();
-})
+});
 
 // Utility Functions
-function randomIntFromRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function randomColor(colors) {
-  return colors[Math.floor(Math.random() * colors.length)];
-}
-
-function distance(x1, y1, x2, y2) {
-  const xDist = x2 - x1;
-  const yDist = y2 - y1;
-
-  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
-}
+function randomIntFromRange(min, max) { return Math.floor(Math.random() * (max - min + 1) + min); }
+function randomColor(colors) { return colors[Math.floor(Math.random() * colors.length)]; }
+function distance(x1, y1, x2, y2) { return Math.sqrt(Math.pow(Math.abs(x1 - x2), 2) + Math.pow(Math.abs(y1 - y2), 2)); }
 
 // Objects
-function Object(x, y, radius, color) {
+function point(x, y) {
+  c.beginPath();
+  c.arc(x, y, 1, 0, 2 * Math.PI, true);
+  c.fill();
+  c.closePath();
+}
+
+function line(x1, y1, x2, y2) {
+  c.beginPath();
+  c.moveTo(x1, y1);
+  c.lineTo(x2, y2);
+  c.stroke();
+  c.closePath();
+}
+
+function Circle(x, y, radius, color) {
   this.x = x;
   this.y = y;
   this.radius = radius;
@@ -61,31 +62,68 @@ function Object(x, y, radius, color) {
   this.draw = () => {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = this.color;
+    c.stroke();
+    c.closePath();
+  }
+}
+
+function Indicator(x, y) {
+  this.x = x;
+  this.y = y;
+  this.color = '#000';
+
+  this.update = () => {
+    this.x = 2 * radius * Math.cos(1 * time);
+    this.y = 2 * radius * Math.sin(1 * time);
+
+    this.draw();
+  }
+
+  this.draw = () => {
+    c.beginPath();
+    c.arc(this.x, this.y, 5, 0, Math.PI * 2, false);
     c.fillStyle = this.color;
     c.fill();
+
+    c.moveTo(0, 0);
+    c.lineTo(this.x, this.y);
+    c.strokeStyle = '#000';
+    c.stroke();
     c.closePath();
   }
 }
 
 // Implementation
-let objects;
+let circle, ind;
 function init() {
-  objects = [];
+  // remap coordinates
+  c.translate(400, canvas.height / 2);
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push();
-  }
+  circle = new Circle(0, 0, radius * 2, '#000');
+  ind = new Indicator(0, 0);
 }
 
 // Animation Loop
+let time = 0;
+let wave = [];
+let radius = 50 * (4 / (1 * Math.PI));
+
 function animate() {
   requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
+  c.clearRect(-canvas.width, -canvas.height, canvas.width * 2, canvas.height * 2);
 
-  c.fillText('HTML CANVAS TEMPLATE', mouse.x, mouse.y);
-  // objects.forEach(object => {
-  //  object.update();
-  // });
+  circle.update();
+  ind.update();
+  wave.unshift(ind.y);
+
+  line(ind.x, ind.y, 200, wave[0]);
+  for (let i = 0; i < wave.length; i++) {
+    point(i + 200, wave[i]);
+  }
+
+  if (wave.length > 800) { wave.pop(); }
+  time += 0.05;
 }
 
 init();
