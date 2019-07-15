4e50d7207ff896f6fb7cbe45b3a157624bd1c598
DevAccNiclas <nxclxs.rist@gmail.com>
2019-07-15 18:49:01 +0200

    add transform method

--- transformation/canvas.js
+++ transformation/canvas.js
@@ -49,9 +49,16 @@ function circle(x, y, radius) {
 }
 
 // Implementation
-function init() { c.translate(300, canvas.height / 2); }
+function init() {
+  c.translate(300, canvas.height / 2);
+  y = [100, 100, 100, -100, -100, -100, 100, 100, 100, -100, -100, -100];
+  fourierY = fourierTransform(y);
+}
 
 // Animation Loop
+let y = [];
+let fourierY;
+
 let time = 0;
 let wave = [];
 let first = true;
@@ -62,28 +69,28 @@ function animate() {
 
   let x = 0, y = 0;
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < fourierY.length; i++) {
     let prevx = x;
     let prevy = y;
 
-    let n = i * 2 + 1;
-    let radius = 100 * (4 / (n * Math.PI));
-
-    x += radius * Math.cos(n * time);
-    y += radius * Math.sin(n * time);
-
-    circle(prevx, prevy, radius, false);
-    line(prevx, prevy, x, y);
+    // let n = i * 2 + 1;
+    // let radius = 100 * (4 / (n * Math.PI));
+    //
+    // x += radius * Math.cos(n * time);
+    // y += radius * Math.sin(n * time);
+    //
+    // circle(prevx, prevy, radius, false);
+    // line(prevx, prevy, x, y);
   }
   wave.unshift(y);
 
-  line(x, y, 200, wave[0]);
-  if (!first) {
-    for (let i = 0; i < wave.length; i++) {
-      point(i + 200, wave[i]);
-      line(i + 200, wave[i], i + 200 + 1, wave[i + 1]);
-    }
-  }
+  // line(x, y, 200, wave[0]);
+  // if (!first) {
+  //   for (let i = 0; i < wave.length; i++) {
+  //     point(i + 200, wave[i]);
+  //     line(i + 200, wave[i], i + 200 + 1, wave[i + 1]);
+  //   }
+  // }
 
   if (wave.length > 900) { wave.pop(); }
   time -= 0.02;
